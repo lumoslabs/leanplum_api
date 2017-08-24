836b6d995a2aa5f4861b587ccdc094a335ae3080
beingmattlevy <matt@beingmattlevy.com>
2017-08-24 14:51:28 -0700

    fix broken specs
    timestamps were off

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = true
   c.cassette_library_dir = 'spec/fixtures/vcr'
   c.hook_into :webmock
 
