46c91a7e4279c6c36a849699d286de7df593430c
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-08-30 07:12:28 -0400

    specs

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe LeanplumApi::Connection do
   let(:http) { described_class.new(LeanplumApi.configuration.production_key) }
+  def argument_string(dev_mode)
+    "appId=#{LeanplumApi.configuration.app_id}&clientKey=#{LeanplumApi.configuration.production_key}&apiVersion=1.0.6&devMode=#{dev_mode}&action=multi&time=#{Time.now.utc.strftime('%s')}"
+  end
 
   context 'regular mode' do
     it 'should build the right multi url' do
-      expect(http.send(:authed_multi_param_string)).to eq(
-        "appId=#{LeanplumApi.configuration.app_id}&clientKey=#{LeanplumApi.configuration.production_key}&apiVersion=1.0.6&devMode=false&action=multi&time=#{Time.now.utc.strftime('%s')}"
-      )
+      expect(http.send(:authed_multi_param_string)).to eq(argument_string(false))
     end
   end
 
@@ -19,9 +20,7 @@ describe LeanplumApi::Connection do
     end
 
     it 'should build the right developer mode url' do
-      expect(http.send(:authed_multi_param_string)).to eq(
-        "appId=#{LeanplumApi.configuration.app_id}&clientKey=#{LeanplumApi.configuration.production_key}&apiVersion=1.0.6&devMode=true&action=multi&time=#{Time.now.utc.strftime('%s')}"
-      )
+      expect(http.send(:authed_multi_param_string)).to eq(argument_string(true))
     end
   end
 end
