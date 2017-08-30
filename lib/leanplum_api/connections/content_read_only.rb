4d5c03f661be755c829371593c24c8d2b11b0482
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-08-30 06:08:42 -0400

    working

--- lib/leanplum_api/connections/content_read_only.rb
+++ lib/leanplum_api/connections/content_read_only.rb
@@ -2,14 +2,5 @@ require 'leanplum_api/connection'
 
 module LeanplumApi::Connections
   class ContentReadOnly < LeanplumApi::Connection
-    def initialize(options = {})
-      raise 'Content read only key not configured!' unless LeanplumApi.configuration.content_read_only_key
-      super
-    end
-
-    # Data export API requests need to use the Data Export key
-    def authentication_params
-      super.merge(clientKey: LeanplumApi.configuration.content_read_only_key)
-    end
   end
 end
--- lib/leanplum_api/connections/data_export.rb
+++ lib/leanplum_api/connections/data_export.rb
@@ -2,14 +2,5 @@ require 'leanplum_api/connection'
 
 module LeanplumApi::Connections
   class DataExport < LeanplumApi::Connection
-    def initialize(options = {})
-      raise 'Data export key not configured' unless LeanplumApi.configuration.data_export_key
-      super
-    end
-
-    # Data export API requests need to use the Data Export key
-    def authentication_params
-      super.merge(clientKey: LeanplumApi.configuration.data_export_key)
-    end
   end
 end
