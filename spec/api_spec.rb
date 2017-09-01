a70b9200556ced9b1f7c2ca82a0b5f700a321811
beingmattlevy <matt@beingmattlevy.com>
2017-08-31 22:23:50 -0700

    devices need to be top level
    moved devices to top level argument to Leanplum

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -56,7 +56,7 @@ describe LeanplumApi::API do
   end
 
   context 'users' do
-    it 'build_user_attributes_hash' do
+    it 'builds user_attributes_hash' do
       expect(api.send(:build_user_attributes_hash, users.first)).to eq({
         userId: first_user_id,
         action: 'setUserAttributes',
@@ -69,6 +69,15 @@ describe LeanplumApi::API do
           is_tipping: true
         )
       })
+    end
+
+    it 'builds user_attributes_hash with device' do
+      user = users.first.merge(devices: devices)
+      user_attributes_hash = api.send(:build_user_attributes_hash, user)
+      expect(user_attributes_hash[:devices].first).to eq({ device_id: 'fu123',
+                                                           appVersion: 'x42x',
+                                                           deviceModel: 'p0d',
+                                                           create_date: '2018-01-01'.to_date }.with_indifferent_access )
     end
 
     context 'set_user_attributes' do
